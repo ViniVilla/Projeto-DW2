024482a90f557ffb72be35fd33325f6d1dfebd5d
ViniVilla <viniciuscastilhovilla@gmail.com>
2019-11-08 11:40:20 -0200

    Adiciona primeira versão do banco de dados

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,37 @@
+drop table if exists usuarios;
+create table usuarios (
+	id identity primary key,
+	username varchar(10) unique not null,
+	password varchar(100) not null,
+	nome varchar(40) not null,
+	email varchar(40) not null
+);
+
+drop table if exists papeis;
+create table papeis (
+	id identity primary key,
+	usuario_id bigint unique,
+	papel varchar(20) unique not null
+);
+alter table papeis add foreign key (usuario_id) references usuarios(id);
+
+
+drop table if exists ordem_servico;
+create table ordem_servico (
+    id identity primary key,
+    endereco_id bigint unique not null,
+    plano varchar(25) not null,
+    data_criacao date not null,
+    data_atribuicao date,
+    data_finalizacao date
+);
+alter table ordem_servico add foreign keu (endereco_id) references endereco(id);
+
+
+drop table if exists endereco;
+create table endereco (
+    id identity primary key,
+    cidade varchar(255) not null,
+    bairro varchar(255) not null,
+    rua varchar(255) not null
+);
