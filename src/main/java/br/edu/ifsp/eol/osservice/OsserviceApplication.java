319e88f93a539b03eb4c4d82fdc70551eb7246a4
ViniVilla <viniciuscastilhovilla@gmail.com>
2019-11-08 11:17:05 -0200

    first commit

---
+++ src/main/java/br/edu/ifsp/eol/osservice/OsserviceApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.eol.osservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsserviceApplication.class, args);
+	}
+
+}
