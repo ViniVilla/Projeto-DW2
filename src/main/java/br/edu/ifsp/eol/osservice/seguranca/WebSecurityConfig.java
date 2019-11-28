feaa21ed5cb7747912eae1474f3010f3360210d8
ViniVilla <viniciuscastilhovilla@gmail.com>
2019-11-28 14:17:04 -0300

    Coloca necessidade de ROLE_USER para criar OS

--- src/main/java/br/edu/ifsp/eol/osservice/seguranca/WebSecurityConfig.java
+++ src/main/java/br/edu/ifsp/eol/osservice/seguranca/WebSecurityConfig.java
@@ -33,7 +33,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/teste")
+                .antMatchers("/ordem-servico")
                 .hasAuthority("ROLE_USER")
                 .antMatchers("/", "/**")
                 .permitAll()
